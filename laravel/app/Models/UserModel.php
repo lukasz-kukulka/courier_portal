890f52cbe8dd24fe653f961572bc35ca7ce7978f
lukasz-kukulka <qqla83@gmail.com>
2023-07-26 19:15:11 +0100

    add user model

---
+++ laravel/app/Models/UserModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'username', 'name', 'surname', 'phone_number', 'd_o_b', 'email', 'password', 'account_type', 'group'
+    ];
+
+    public function userAnnouncement() {
+        return $this->hasMany( UserAnnouncementDataModel::class, 'author');
+    }
+}
