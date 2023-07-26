38612d2a83a8af357d7b188d52c6abbcc9b9f0b1
lukasz-kukulka <qqla83@gmail.com>
2023-07-26 19:12:06 +0100

    complete cargo detail form

---
+++ laravel/resources/views/cargo_details_create_form.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('add_header')
+    {{-- <script src="{{ asset('js/accounts_scripts.js') }}"></script> --}}
+    {{-- <link rel="stylesheet" href="{{ asset('css/accounts_form_styles.css') }}"> --}}
+    @php
+        $JsonParserController = app(\App\Http\Controllers\JsonParserController::class);
+        $cargoData = $JsonParserController->cargoAction();
+    @endphp
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('base.user_announcement_card_name') }}</div>
+                        <div class="card-body">
+                            <form action="{{ route('user_announcement.store') }}" method="POST" id="user_announcement_form_cargo_details">
+                                @csrf
+                                @foreach ($cargoData[ 'cargo_types' ] as $cargo_type )
+                                    <x-cargo_details_component :name="$cargo_type[ 'id' ]" :number="$_POST[ $cargo_type[ 'id' ] ]" :params="json_encode( $cargo_type[ 'params' ] )" />
+                                @endforeach
+                                <div class="row mb-0">
+                                    <div class="col text-end">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('base.next') }}
+                                        </button>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+@endsection
