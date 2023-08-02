2466913a60e43d64e1f025f69199364c81f19b0d
lukasz-kukulka <qqla83@gmail.com>
2023-08-02 05:39:55 +0100

    add other model class

--- laravel/app/Models/OtherAnnouncement.php
+++ laravel/app/Models/OtherAnnouncement.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class OtherAnnouncement extends Model
 {
     use HasFactory;
+    protected $table = 'others_parameters';
+    protected $fillable = [
+        'announcement_id', 'description'
+    ];
+    public function announcementId() {
+        return $this->belongsTo( UserAnnouncement::class, 'announcement_id' );
+    }
 }
