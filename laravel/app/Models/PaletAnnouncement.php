7decba3bd8ae062cd56d2674ecdf098ab3e18c7f
lukasz-kukulka <qqla83@gmail.com>
2023-08-02 05:40:35 +0100

    add pallets model class

--- laravel/app/Models/PaletAnnouncement.php
+++ laravel/app/Models/PaletAnnouncement.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class PaletAnnouncement extends Model
 {
     use HasFactory;
+    protected $table = 'pallets_parameters';
+    protected $fillable = [
+        'announcement_id', 'weight', 'length', 'width', 'height'
+    ];
+    public function announcementId() {
+        return $this->belongsTo( UserAnnouncement::class, 'announcement_id' );
+    }
 }
