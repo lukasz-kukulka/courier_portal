c2221d9cb16521542805ada11ac38ce89860633f
lukasz-kukulka <qqla83@gmail.com>
2023-08-09 05:36:04 +0100

    refactor add separate translate and add new

--- laravel/resources/views/partials/nav_menu_element.blade.php
+++ laravel/resources/views/partials/nav_menu_element.blade.php
@@ -1,15 +1,15 @@
 @if ( $item['type'] == "standard" )
     <li class="nav-item">
-        <a class="nav-link" href="{{ route( $item['route_name'] ) }}">{{ __( $item['name'] ) }}</a>
+        <a class="nav-link" href="{{ route( $item['route_name'] ) }}">{{ __( 'base.' . $item['name'] ) }}</a>
     </li>
     @elseif ( $item['type'] == "dropdown" )
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {{ $item['name'] }}
+                {{ __( 'base.' . $item['name'] )  }}
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             @foreach ( $item[ 'dropdown_elements' ] as $subelement )
-                <a class="dropdown-item" href="{{ route( $subelement['route_name'] ) }}">{{ $subelement['name'] }}</a>
+                <a class="dropdown-item" href="{{ route( $subelement['route_name'] ) }}">{{ __( 'base.' . $subelement['name'] ) }}</a>
                 <div class="dropdown-divider"></div>
             @endforeach
             </div>
