b9c4cbd10aad13aeed69f5d0aea397215dbacc45
lukasz-kukulka <qqla83@gmail.com>
2023-08-13 18:34:43 +0100

    add user announcemet commad delete old posts

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -13,6 +13,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        //$schedule->command('app:user-announcement-limit-time')->dailyAt("00:00");
+
+        $schedule->command('app:user-announcement-limit-time')->everyMinute();
     }
 
     /**
