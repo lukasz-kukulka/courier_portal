8515e2c8bee1e729ac3724f7e48091a5b96fa982
lukasz-kukulka <qqla83@gmail.com>
2023-08-16 14:40:48 +0100

    add courie announcement images model

---
+++ laravel/database/migrations/2023_08_15_171721_courier_announcement_images.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void {
+        Schema::create('courier_announcement_images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('courier_announcement_id'); // klucz obcy
+            $table->string('image_name');
+            $table->string('image_link');
+            $table->string('image_description');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('courier_announcement_images');
+    }
+};
