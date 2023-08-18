02eae6bf5aae2dd61166e8b1c1e4c4d63a765d1d
lukasz-kukulka <qqla83@gmail.com>
2023-08-18 15:12:11 +0100

    add courier post codes uk archive migration

---
+++ laravel/database/migrations/courier_announcement/2023_08_18_141100_post_codes_uk_archive.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void {
+        Schema::create('post_codes_uk_archive', function (Blueprint $table) {
+            $json = app(\App\Http\Controllers\JsonParserController::class)->ukPostCodeAction();
+
+            $table->id();
+            $table->unsignedBigInteger('author'); // klucz obcy
+            foreach( $json as $post_code ) {
+                $table->boolean( $post_code )->default( false );
+            }
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists( 'post_codes_uk_archive' );
+    }
+};
