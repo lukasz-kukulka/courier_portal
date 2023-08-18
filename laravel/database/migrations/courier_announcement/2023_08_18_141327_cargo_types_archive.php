594a394f2803f512f77744865920055a8c099331
lukasz-kukulka <qqla83@gmail.com>
2023-08-18 15:15:04 +0100

    add courier cargo type archive migration

---
+++ laravel/database/migrations/courier_announcement/2023_08_18_141327_cargo_types_archive.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void {
+        Schema::create('cargo_types_archive', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('courier_announcement_id'); // klucz obcy
+            $table->string('cargo_name');
+            $table->unsignedInteger('cargo_price');
+            $table->string('cargo_description');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('cargo_types_archive');
+    }
+};
