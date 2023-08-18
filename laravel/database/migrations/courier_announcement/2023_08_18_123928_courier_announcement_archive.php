141215bcfdbecce07d1bfcf81e574beee89178fe
lukasz-kukulka <qqla83@gmail.com>
2023-08-18 13:42:13 +0100

    add courier announcement archive migration

---
+++ laravel/database/migrations/courier_announcement/2023_08_18_123928_courier_announcement_archive.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void {
+        Schema::create('courier_announcement_archive', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author'); // klucz obcy
+            $table->unsignedBigInteger('description');
+            $table->date('experience_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists( 'courier_announcement_archive' );
+    }
+};
