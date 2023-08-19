84289e615453cbc89bae09e9832e8de236a30968
lukasz-kukulka <qqla83@gmail.com>
2023-08-19 12:14:36 +0100

    add courier announcement travel dates archive migration

---
+++ laravel/database/migrations/courier_announcement/2023_08_19_111348_courier_announcement_travel_dates_archive.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void {
+        Schema::create('courier_announcement_travel_dates_archive', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('courier_announcement_id'); // klucz obcy
+            $table->string('direction');
+            $table->date('date');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('courier_announcement_travel_dates_archive');
+    }
+};
