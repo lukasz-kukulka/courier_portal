742b1331fed3714b53608355bf831e9224e74955
lukasz-kukulka <qqla83@gmail.com>
2023-08-20 05:58:31 +0100

    add courier announcement travel dates archive model

---
+++ laravel/app/Models/CourierAnnouncement/CourierTravelDateArchive.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CourierTravelDateArchive extends Model
+{
+    use HasFactory;
+    protected $table = 'courier_announcement_travel_dates_archive';
+    protected $fillable = [
+        'courier_announcement_id', 'direction', 'date', 'description'
+    ];
+    public function announcementId() {
+        return $this->belongsTo( CourierAnnouncementArchive::class, 'courier_announcement_id' );
+    }
+}
