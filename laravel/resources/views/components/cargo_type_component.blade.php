b3eff1feb69d6c8285618125f2110c729b23273d
lukasz-kukulka <qqla83@gmail.com.com>
2023-09-05 05:07:11 +0100

    add date component class

--- laravel/resources/views/components/cargo_type_component.blade.php
+++ laravel/resources/views/components/cargo_type_component.blade.php
@@ -34,10 +34,3 @@
 
     </td>
 </tr>
-
-{{-- @error( "xxx" )
-            <span class="invalid-feedback" role="alert">
-                <strong> {{ $message }}</strong>
-            </span>
-@enderror --}}
-
